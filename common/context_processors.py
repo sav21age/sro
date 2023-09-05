40ad5b5ba1e53063a56c4ae39736cdfc45cda5d7
sav21age <sav21age@gmail.com>
2023-09-05 07:22:11 +0300

    Fix errors and bugs

--- common/context_processors.py
+++ common/context_processors.py
@@ -3,7 +3,6 @@ from django.conf import settings
 def main(request):
     context = {
       'debug': settings.DEBUG,
-      # 'cache_timeout': int(settings.CACHE_TIMEOUT),
       'rel_canonical': request.build_absolute_uri(),
       'host': f"{request.is_secure() and 'https' or 'http'}://{request.get_host()}"
     }
