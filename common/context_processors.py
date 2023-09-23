e1378d885533aa999ea023b18f1b101353b5f36d
sav21age <sav21age@gmail.com>
2023-09-23 13:54:04 +0300

    Fix rel_canonical

--- common/context_processors.py
+++ common/context_processors.py
@@ -3,7 +3,7 @@ from django.conf import settings
 def main(request):
     context = {
       'debug': settings.DEBUG,
-      'rel_canonical': request.build_absolute_uri(),
+      'rel_canonical': request.build_absolute_uri(request.path),
       'host': f"{request.is_secure() and 'https' or 'http'}://{request.get_host()}"
     }
     return context
